b3a2ad62d2ae2b24b5690a5f27afa705f1b70be9
Lucas Crijns <lucascrijns@gmail.com>
2022-12-03 21:47:13 +0100

    Add api calls

--- website/government.js
+++ website/government.js
@@ -10,3 +10,32 @@ async function buttonClick() {
     console.log("Response", dataStr);
     console.log("Request finished!");
 }
+
+async function issueCredential(address, connection_id) {
+    let result = await $.post(getEndpoint("8100", "issue/process"), data = {
+        "connectionId": connection_id,
+        "credentialDefinitionId": CREDENTIAL_DEFINITION_ID,
+        "atributes": {
+            "address": address
+        }
+    });
+    return result;
+}
+
+async function checkIssueState(process_id) {
+    let result = await $.get(getEndpoint("8100", `issue/${process_id}/state`));
+    return result;
+}
+
+async function doCreateCredential() {
+    let address = $("#address").val();
+    let result = await issueCredential(address, CONNECTION_ID);
+    let wait = (seconds) => 
+        new Promise(resolve => 
+            setTimeout(() => resolve(true), seconds * 1000)
+        );
+    while (checkIssueState(result.processId) == '"VC_ISSUED"') {
+        await wait(1);
+    }
+    alert("Done!")
+}
