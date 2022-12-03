e6a0acd71cfee533c5d6f026241485d2a9cf39c4
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-12-03 23:00:39 +0100

    Update credential definition

--- website/service.js
+++ website/service.js
@@ -13,19 +13,40 @@ async function buttonClick() {
     console.log("Request finished!");
 }
 
+function onUpdateConnectButton(isConnecting) {
+    let button = $("#sendStatements");
+
+    if (!isConnecting && getConnectionId()) {
+        button.removeAttr("disabled");
+    } else {
+        button.attr("disabled", true);
+    }
+}
+
 async function sendBankStatements() {
     // fetch address
-    const { processId } = await $.post(getEndpoint("8081", "verify/process"), {
-        connectionId: connection_id,
-        credentialDefinitionId: CREDENTIAL_DEFINITION_ID,
-        attributes: [CLAIM_KEY],
-    });
+    const { processId } = await fetch(getEndpoint("8081", "verify/process"), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            connectionId: getConnectionId(),
+            credentialDefinitionId: CREDENTIAL_DEFINITION_ID,
+            attributes: [CLAIM_KEY],
+        }),
+    }).then((response) => response.json());
 
     // wait for fetch to progress
     let status;
     do {
         status = await $.get(getEndpoint("8081", `verify/process/${processId}/state`));
-    } while (status !== "IN_PROGRESS");
+
+        console.log("connection status update", status);
+        await sleep(1000);
+    } while (status === "IN_PROGRESS");
+
+    console.log("connection status final update", status);
 
     // get claim
     if (status === "VERIFIED") {
