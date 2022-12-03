ac938c04d7fd1dfb046492534615c16db95afdb9
Lucas Crijns <lucascrijns@gmail.com>
2022-12-04 00:16:50 +0100

    Add government better

--- website/government.js
+++ website/government.js
@@ -2,6 +2,9 @@ const CONNECTION_ID = "government_connection_id";
 
 $(() => {
     console.log(getEndpoint(8080, "government"));
+    if ("currentAddress" in localStorage) {
+        $("#currentAddress").text(localStorage["currentAddress"]);
+    }
 });
 
 async function buttonClick() {
@@ -31,7 +34,12 @@ async function issueCredential(address, connection_id) {
 }
 
 async function doCreateCredential() {
-    $("#submission").attr("disabled", true);
+    try {
+        doRevoke()
+    } catch (e) {
+        console.log(e)
+    }
+    // $("#submission").attr("disabled", true);
     let address = $("#address").val();
     let result = await issueCredential(address, getConnectionId());
     console.log("Issued it");
@@ -43,4 +51,28 @@ async function doCreateCredential() {
         console.log(status)
     } while (status != "VC_ISSUED")
     alert("Accepted credential");
+    $("#currentAddress").text(address);
+    localStorage["currentAddress"] = address;
+}
+
+async function doRevoke() {
+    let active_thingy = await $.get(getEndpoint("9020", `revocation/active-registry/${CREDENTIAL_DEFINITION_ID}`));
+    console.log(active_thingy);
+    let revoc_id = active_thingy.result.revoc_reg_id;
+
+    let details = await $.get(getEndpoint("9020", `revocation/registry/${revoc_id}/issued/details`));
+    console.log(details)
+    let cred_ex_id = details[0].cred_ex_id;
+
+    let result = await fetch(getEndpoint("9020", "revocation/revoke"), {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            connection_id: getConnectionId(),
+            cred_ex_id: cred_ex_id
+    })});
+    console.log(result)
 }
